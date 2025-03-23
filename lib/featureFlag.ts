2f7e06eaada6db21c770e95af9d8909df62635c9
Favot <70208301+Favot@users.noreply.github.com>
2025-03-23 23:02:38 +0000

    Update env config

--- lib/featureFlag.ts
+++ lib/featureFlag.ts
@@ -1,10 +1,9 @@
-import { FeatureFlagKey } from '@/domain';
+import { FeatureFlagKey } from '@/domain'
 
-const strToBoolean = (value: string | undefined): boolean => true;
-//   value?.toLowerCase() === "true" // TODO: Implement environment variables
+const strToBoolean = (value: string | undefined): boolean => value?.toLowerCase() === 'true'
 
 export const BOOLEAN_FEATURE_FLAG_MAP: Record<FeatureFlagKey, boolean> = {
-  FF_ALWAYS_FALSE: strToBoolean(process.env.NEXT_PUBLIC_FF_ALWAYS_FALSE),
-  FF_ALWAYS_TRUE: strToBoolean(process.env.NEXT_PUBLIC_FF_ALWAYS_TRUE),
-  FF_SESSION_MOCK: strToBoolean(process.env.NEXT_PUBLIC_FF_SESSION_MOCK),
-};
+  FF_ALWAYS_FALSE: strToBoolean(process.env.EXPO_PUBLIC_FF_ALWAYS_FALSE),
+  FF_ALWAYS_TRUE: strToBoolean(process.env.EXPO_PUBLIC_FF_ALWAYS_TRUE),
+  FF_SESSION_MOCK: strToBoolean(process.env.EXPO_PUBLIC_FF_SESSION_MOCK),
+}

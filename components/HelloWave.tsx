dd4f1c54bd8c2a06137f8123dda741658dd27e7a
Favot <70208301+Favot@users.noreply.github.com>
2025-03-24 10:09:22 +0000

    Setup Nativewind

--- components/HelloWave.tsx
+++ components/HelloWave.tsx
@@ -1,36 +1,37 @@
-import { useEffect } from 'react'
-import { StyleSheet } from 'react-native'
+import { useEffect } from 'react';
+import { StyleSheet, Text } from 'react-native';
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
   withRepeat,
   withSequence,
   withTiming,
-} from 'react-native-reanimated'
+} from 'react-native-reanimated';
 
-import { ThemedText } from '@/components/ThemedText'
-import { useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next';
 
 export function HelloWave() {
-  const { t } = useTranslation()
-  const rotationAnimation = useSharedValue(0)
+  const { t } = useTranslation();
+  const rotationAnimation = useSharedValue(0);
 
   useEffect(() => {
     rotationAnimation.value = withRepeat(
       withSequence(withTiming(25, { duration: 150 }), withTiming(0, { duration: 150 })),
       4 // Run the animation 4 times
-    )
-  }, [])
+    );
+  }, []);
 
   const animatedStyle = useAnimatedStyle(() => ({
     transform: [{ rotate: `${rotationAnimation.value}deg` }],
-  }))
+  }));
 
   return (
     <Animated.View style={animatedStyle}>
-      <ThemedText style={styles.text}>{t('common.welcome')}👋</ThemedText>
+      <Text style={styles.text} className="text-red-200">
+        {t('common.welcome')}👋
+      </Text>
     </Animated.View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -39,4 +40,4 @@ const styles = StyleSheet.create({
     lineHeight: 32,
     marginTop: -6,
   },
-})
+});
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: [
+    './app/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx.ts,tsx}',
+    './constants/**/*.{js,jsx.ts,tsx}',
+    './constext/**/*.{js,jsx.ts,tsx}',
+    './domain/**/*.{js,jsx.ts,tsx}',
+    './gateway/**/*.{js,jsx.ts,tsx}',
+    './hooks/**/*.{js,jsx.ts,tsx}',
+    './i18n/**/*.{js,jsx.ts,tsx}',
+    './lib/**/*.{js,jsx.ts,tsx}',
+  ],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
